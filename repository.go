e29b16627728bdfa0fc8ebd621649914d906a70a
eylultanriverdi <eylulrabiatanriverdi@gmail.com>
2023-06-08 01:39:54 +0300

    profile token added

--- repository.go
+++ repository.go
@@ -335,7 +335,7 @@ func (repository *Repository) GetByID(userID string) (*models.User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	filter := bson.M{"_id": userID}
+	filter := bson.M{"uid": userID}
 
 	entity := models.UserEntity{}
 	err := collection.FindOne(ctx, filter).Decode(&entity)
